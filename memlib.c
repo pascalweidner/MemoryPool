5b1c43f1a0a0d8a9037c4efec9a2dfc7939afcfd
pasca <pascal.pw05@gmail.com>
2024-06-30 22:32:44 +0200

    allocates new memory

--- memlib.c
+++ memlib.c
@@ -9,6 +9,7 @@ struct header
     int free;
     header *next;
     header *prev;
+    int fixed;
 };
 
 size_t META_SIZE = sizeof(header);
@@ -27,12 +28,38 @@ void initPool()
     glob_head = (header *)mem;
     glob_head->size = 64 * 100 - META_SIZE;
     glob_head->free = 1;
+    glob_head->fixed = 1;
     glob_head->next = NULL;
     glob_head->prev = NULL;
 }
 
+void *alloc_new(size_t size)
+{
+    if (size < 3200 - META_SIZE)
+    {
+        size = 3200;
+    }
+    else
+    {
+        size += META_SIZE;
+    }
+
+    void *mem = (void *)VirtualAlloc(NULL, (size), MEM_COMMIT, PAGE_READWRITE);
+    if (mem == NULL)
+    {
+        printf("Error");
+        exit(EXIT_FAILURE);
+    }
+
+    header *curr = (header *)mem;
+    curr->size = size - META_SIZE;
+
+    return mem;
+}
+
 void *my_malloc(size_t size)
 {
+    header *prev = NULL;
     header *curr = glob_head;
     while (curr != NULL)
     {
@@ -40,25 +67,41 @@ void *my_malloc(size_t size)
         {
             break;
         }
+        prev = curr;
         curr = curr->next;
     }
-    header *next = (header *)((int)curr + size + META_SIZE);
-    if (curr->next != next)
+
+    if (curr == NULL)
     {
-        next->prev = curr;
-        next->next = curr->next;
-        next->free = 1;
-        next->size = curr->size - size - META_SIZE;
+        void *mem = alloc_new(size);
+        curr = (header *)mem;
+        prev->next = curr;
+        curr->prev = prev;
+        curr->next = NULL;
+        curr->fixed = 1;
     }
 
-    if (curr->next != NULL)
+    if (curr->next != NULL || size != curr->size)
     {
-        curr->next->prev = next;
+        header *next = (header *)((int)curr + size + META_SIZE);
+        if (curr->next != next)
+        {
+            next->prev = curr;
+            next->next = curr->next;
+            next->free = 1;
+            next->size = curr->size - size - META_SIZE;
+            next->fixed = 0;
+        }
+
+        if (curr->next != NULL)
+        {
+            curr->next->prev = next;
+        }
+        curr->next = next;
     }
 
     curr->size = size;
     curr->free = 0;
-    curr->next = next;
 
     return (void *)((int)curr + META_SIZE);
 }
@@ -67,6 +110,10 @@ void my_free(void *address)
 {
     header *curr = (header *)((int)address - META_SIZE);
     curr->free = 1;
+    if (curr->fixed == 1)
+    {
+        return;
+    }
     if (curr->prev != NULL && curr->prev->free == 1)
     {
         curr->next->prev = curr->prev;
@@ -91,9 +138,44 @@ void my_free(void *address)
 int main()
 {
     initPool();
-    int *p = (int *)my_malloc(5000);
+    int *p = (int *)my_malloc(6400 - sizeof(header));
     printf("mem_p=%p \n", (int)p);
-    int *p2 = (int *)my_malloc(1000);
+
+    header *curr = glob_head;
+    while (curr != NULL)
+    {
+        printf("mem=%p size=%d \n", curr, curr->size);
+        curr = curr->next;
+    }
+
+    int *p2 = (int *)my_malloc(5000);
+    printf("mem_p=%p \n", (int)p2);
+
+    curr = glob_head;
+    while (curr != NULL)
+    {
+        printf("mem=%p size=%d \n", curr, curr->size);
+        curr = curr->next;
+    }
+
+    int *p3 = (int *)my_malloc(4000);
+    printf("mem_p=%p \n", (int)p2);
+
+    curr = glob_head;
+    while (curr != NULL)
+    {
+        printf("mem=%p size=%d \n", curr, curr->size);
+        curr = curr->next;
+    }
+
+    my_free(p3);
+
+    curr = glob_head;
+    while (curr != NULL)
+    {
+        printf("mem=%p size=%d \n", curr, curr->size);
+        curr = curr->next;
+    }
 
     VirtualFree((void *)glob_head, 64 * 100, MEM_DECOMMIT);
 
